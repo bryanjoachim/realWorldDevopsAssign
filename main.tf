188b53282312cc625bfdb558087464fbc730fda4
bryanjoachim <33373881+bryanjoachim@users.noreply.github.com>
2025-07-15 14:07:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,11 +43,11 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "a" {
   subnet_id = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public_a.id
+  route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "b" {
   subnet_id = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public_b.id
+  route_table_id = aws_route_table.public.id
 }
 
 
